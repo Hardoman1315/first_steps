84510fd38deb72dd6010df027cb9219c6083a471
Hardoman1315 <hardomangithub@gmail.com>
2024-10-16 23:27:54 +0400

    Complete some unfinished code, added an example of using encapsulation functions

--- classes.py
+++ classes.py
@@ -16,24 +16,27 @@ class ForgeItem(abc.ABC):
         pass
 
     @staticmethod
-    def total_forged_count() -> int:
+    def total_forged_count(amount) -> int:
         for _ in range(amount):
             ForgeItem.total_forged += 1
-            return forge_item.total_forged
+        return ForgeItem.total_forged
 
 
 class Materials(ForgeItem):
     def __init__(self, forging_time: float, amount: int, cost: float, material: float):
         super().__init__(forging_time, amount)
         self.cost : float = cost
-        self.material : float= material
+        self.material : float = material
         self.total_materials : float = self.material * self.amount
         self.total_currencies : float = self.cost * self.amount
 
     def discount(self) -> str:
         if self.amount >= 5:
             self.total_materials = self.total_materials * 0.9
-            return f'Discount for forging multiple items is 10% and total amount of required materials - {self.total_materials}'
+            return (
+                f"Discount for forging multiple items is 10% "
+                f"and total amount of required materials - {self.total_materials}"
+            )
         else:
             return 'Not enough items in forge to get discount'
 
@@ -42,7 +45,8 @@ class Materials(ForgeItem):
 
 
 class AvailableMaterials(Materials):
-    def __init__(self, forging_time: float, amount: int, cost: float, material: float, available_currencies: float, available_materials: float):
+    def __init__(self, forging_time: float, amount: int, cost: float, material: float,
+                available_currencies: float, available_materials: float):
         super().__init__(forging_time, amount, cost, material)
         self.__available_currencies : float = available_currencies
         self._available_materials : float = available_materials
@@ -50,9 +54,9 @@ class AvailableMaterials(Materials):
     def get_available_material(self) -> float:
         return self._available_materials
 
-    def set_available_material(self) -> str:
+    def set_available_material(self, available_materials: float) -> str:
         self._available_materials = available_materials
-        return f'AvailableMaterial now equal {self._available_materials}'
+        return f'Available material now equal {self._available_materials}'
 
     @property
     def get_available_currencies(self) -> float:
@@ -61,9 +65,11 @@ class AvailableMaterials(Materials):
     @get_available_currencies.setter
     def get_available_currencies(self, available_currencies: float):
         self.__available_currencies = available_currencies
+        print(f'Available currencies now equal {self.__available_currencies}')
 
     def possibility_of_forge(self) -> str:
-        if self.__available_currencies < self.total_currencies or self._available_materials < self.total_materials:
+        if (self.__available_currencies < self.total_currencies or
+            self._available_materials < self.total_materials):
             return 'Not enough resources to forge new item'
         else:
             return 'Sufficient resources to forge new item'
@@ -71,14 +77,13 @@ class AvailableMaterials(Materials):
 
 # Uncomment to manual input
 
-"""
-forging_time = float(input("Enter forging forging_time: "))
-amount = int(input("Enter amount of forging: "))
-cost = float(input("Enter cost of single forging: "))
-material = float(input("Enter required amount of material to forge: "))
-available_currencies = float(input("Enter available amount of currencies: "))
-available_materials = float(input("Enter available amount of material: "))
-"""
+# forging_time = float(input("Enter forging forging_time: "))
+# amount = int(input("Enter amount of forging: "))
+# cost = float(input("Enter cost of single forging: "))
+# material = float(input("Enter required amount of material to forge: "))
+# available_currencies = float(input("Enter available amount of currencies: "))
+# available_materials = float(input("Enter available amount of material: "))
+
 
 # Uncomment to random input
 
@@ -88,10 +93,13 @@ cost = random.uniform(1, 10)
 material = random.uniform(1, 10)
 available_currencies = random.uniform(1, 100)
 available_materials = random.uniform(1, 100)
-
-available = AvailableMaterials(forging_time, amount, cost, material, available_currencies, available_materials)
+available = AvailableMaterials(forging_time, amount, cost, material,
+                               available_currencies, available_materials)
 
 print(available.total_time())
 print(available.resources())
 print(available.discount())
+print(available.get_available_material())
+print(available.set_available_material(80))
+available.get_available_currencies = 80
 print(available.possibility_of_forge())
