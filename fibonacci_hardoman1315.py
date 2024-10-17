987b9d13726ceab0a9b188e4bcb95e9588a901c2
Hardoman1315 <hardomangithub@gmail.com>
2024-10-17 23:37:39 +0400

    Split fibonacci function to check_prime and fibonacci, split file manipulation methods, fixed some typos

--- fibonacci_hardoman1315.py
+++ fibonacci_hardoman1315.py
@@ -1,26 +1,36 @@
-def fibonacci(num: int) -> int:
+def check_prime(num: int) -> bool:
     if num == 2:
-        isPrime = True
+        is_prime = True
     elif num <= 1 or num % 2 == 0:
-        isPrime = False
+        is_prime = False
     else:
-        isPrime = True
+        is_prime = True
         for i in range(3, int(num ** 0.5) + 1, 2):
-            if num % i == 0:
-                isPrime = False
-                break
-            isPrime = True
-    if isPrime:
-        lastDigit = num % 10
+                 if num % i == 0:
+                     is_prime = False
+                     break
+                 is_prime = True
+    return is_prime
+
+
+def fibonacci(num: int) -> int:
+    is_prime = check_prime(num)
+    if is_prime:
+        last_digit = num % 10
         previous, current = 0, 1
-        for _ in range(lastDigit):                    
+        for _ in range(last_digit):
             previous, current = current, previous + current
         return previous
 
-with open('in_nums.txt': str, 'r') as input_file:
-    with open('out_nums.txt': str, 'w') as output_file:
-        for line in input_file:
-            currentLine = int(line.strip())
-            fibonacciNum = fibonacci(currentLine)
-            if fibonacciNum is not None:
-                output_file.write(str(fibonacciNum) + '\n')
+
+nums = []
+
+with open('in_nums.txt', 'r') as input_file:
+    for line in input_file:
+        nums.append(int(line.strip()))
+
+with open('out_nums.txt', 'w') as output_file:
+    for number in nums:
+        fib_num = fibonacci(number)
+        if fib_num is not None:
+            output_file.write(str(fib_num) + '\n')
