bb676f222ae97c1deccae72f109964e37b8227b7
Hardoman1315 <hardomangithub@gmail.com>
2024-10-23 17:03:50 +0400

    Removed manual directory input

--- program.py
+++ program.py
@@ -1,9 +1,5 @@
 import json
 import csv
-import os
-
-
-is_existed = False
 
 
 def conditions_complete(users) -> dict:
@@ -22,25 +18,13 @@ def conditions_complete(users) -> dict:
 
 
 def extract_file() -> dict:
-    global is_existed
-    while not is_existed:
-        path_to_file = input("Укажите директорию файла \'in.json\',"
-                             "или оставьте строку пустой,если файл находится в этой же директории \n")
-        path_to_file = path_to_file + "in.json"
-        if not os.path.exists(path_to_file):
-            print(f"Указанного пути не существует. Попробуйте ещё раз")
-        else:
-            is_existed = True
-    with open(path_to_file, "r") as input_file:
+    with open("in.json", "r") as input_file:
         user_data = json.load(input_file)
         return user_data
 
 
 def save_to_CSV(valid_list):
-    path_to_file = input("Укажите директорию для создания файла \'out.csv\',"
-                 "или оставьте строку пустой,если файл находится в этой же директории \n")
-    path_to_file = path_to_file + "out.csv"
-    with open(path_to_file, "w") as output_file:
+    with open("out.csv", "w") as output_file:
         writer = csv.writer(output_file)
         writer.writerow(['name', ' address', ' mail'])
         for user in valid_list:
